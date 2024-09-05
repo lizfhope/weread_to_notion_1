af3a06b89948b7afa217c533206b3836980f6a24
malinkang <malinkang@adsconflux.com>
2024-09-05 11:30:16 +0800

    fix cover

--- weread.py
+++ weread.py
@@ -400,7 +400,7 @@ def update_cover():
     for index, item in enumerate(items):
         print(f"一共{len(items)}个，正在更新第{index+1}个")
         if item.get("properties"):
-            cover = item.get("properties").get("附件").get("files")[0]
+            cover = item.get("properties").get("附件").get("files")[0].pop("name")
             print(f"id = {item.get('id')} cover = {cover}")
             client.databases.update(item.get("id"), cover=cover)
 
